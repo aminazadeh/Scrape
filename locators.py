63ffc52ec4f650eeb7f95cdc5ddfd320f60c5f3e
Mehdi Aminazadeh <mehdi.2000.aminazadeh@gmail.com>
2022-07-27 17:39:18 +0430

    Delete locators.py

--- locators.py
+++
@@ -1,4 +0,0 @@
-class PageLocators:
-    DIV = 'post-card-item kt-col-6 kt-col-xxl-4'
-    DESC = 'kt-post-card__body h2'
-    SRC = 'kt-post-card-thumbnail picture.kt-image-block kt-image-block--rounded img'
